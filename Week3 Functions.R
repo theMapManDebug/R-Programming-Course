a25e9a5f769bdf21d262f385cf8701e8e2c1dc78
theMapManDebug <60194706+theMapManDebug@users.noreply.github.com>
2020-01-22 13:31:51 -0600

    Add files via upload
    Adding makeCacheMatrix and cacheSolve

---
+++ Week3 Functions.R
@@ -0,0 +1,39 @@
+    # Create a special matrix and return its inverse
+    # Test Data: testData <- matrix(rnorm(16), 4, 4)
+
+    # makeCacheMatrix - Creates a special matrix object
+    makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        insert_matrix <- function(new_data) {
+            x <<- new_data
+            inv <<- NULL
+        }
+        get_data <- function() {
+            return(as.matrix(x))
+        }
+        set_inv <- function(inverse) {
+            inv <<- inverse
+        }
+        get_inv <- function() {
+            return(inv)
+        }
+        list(
+            insert_matrix = insert_matrix,
+            get_data = get_data,
+            set_inv = set_inv,
+            get_inv = get_inv
+        )
+    }
+    
+    # cacheSolve - Returns inverse of the matrix created by makeCacheMatrix
+    cacheSolve <- function(x, ...) {
+        inv <- x$get_inv()
+        if (!is.null(inv)) {
+            message("returning inverse from cache...")
+            return(inv)
+        }
+        data <- x$get_data()
+        inv <- solve(data, ...)
+        x$set_inv(inv)
+        return(inv)
+    }
